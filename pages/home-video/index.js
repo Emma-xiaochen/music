b6d4f5c4722cf20458b8e2cdfc1895f946ed943a
Emma-xiaochen <736202625@qq.com>
2022-01-01 14:04:19 +0800

    项目搭建和配置

--- pages/home-video/index.js
+++ pages/home-video/index.js
@@ -7,7 +7,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    topMVs: []
+    topMVs: [],
+    hasMore: true
   },
 
   /**
@@ -15,7 +16,61 @@ Page({
    * async await
    */
   onLoad: async function (options) {
-    const res = await getTopMV(0);
-    this.setData({ topMVs: res.data });
+    // const res = await getTopMV(0)
+    // this.setData({ topMVs: res.data })
+
+    this.getTopMVData(0)
+  },
+  // 封装网络请求的方法
+  getTopMVData: async function(offset) {
+    // 判断是否可以请求
+    if(!this.data.hasMore) return;
+
+    // 展示加载动画
+    wx.showNavigationBarLoading();
+    if(offset === 0) {
+      wx.stopPullDownRefresh()
+    }
+    // 真正请求数据
+    const res = await getTopMV(offset);
+    let newData = this.data.topMVs;
+    if(offset === 0) {
+      newData = res.data;
+    } else {
+      newData = newData.concat(res.data);
+    }
+    // 设置数据
+    this.setData({ topMVs: newData });
+    this.setData({ hasMore: res.hasMore })
+    wx.hideNavigationBarLoading();
+    if (offset === 0) {
+      wx.stopPullDownRefresh();
+    }
+  },
+  // 封装事件处理的方法
+  handleVideoItemClick: function(event) {
+    // 获取id
+    const id = event.currentTarget.dataset.item.id;
+    console.log(id);
+    // 页面跳转
+    wx.navigateTo({
+      url: '/pages/detail-video/index?id=' + id,
+    })
+  },
+  // 其他的生命周期回调函数
+  // 下拉刷新
+  onPullDownRefresh: async function() {
+    // const res = await getTopMV(0)
+    // this.setData({ topMVs: res.data })
+    this.getTopMVData(0)
+  },
+  // 下拉加载更多
+  onReachBottom: async function() {
+    console.log(1);
+    // if(!this.data.hasMore) return;
+    // const res = await getTopMV(this.data.topMVs.length);
+    // this.setData({ topMVs: this.data.topMVs.concat(res.data) })
+    // this.setData({ hasMore: res.hasMore });
+    this.getTopMVData(this.data.topMVs.length);
   }
 })

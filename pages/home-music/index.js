9fed81b5df9a4fcc3a35daf43a4f73e727ac974d
Emma-xiaochen <736202625@qq.com>
2022-01-02 15:05:27 +0800

    状态管理

--- pages/home-music/index.js
+++ pages/home-music/index.js
@@ -1,39 +1,47 @@
 // pages/home-music/index.js
+import { getBanners } from '../../service/api_music'
+import queryRect from '../../util/query-rect'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    swiperHeight: 0,
+    banners: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 获取页面数据
+    this.getPageData();
+    console.log(1);
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 网络请求
+  getPageData: function() {
+    getBanners().then(res => {
+      this.setData({ banners: res.banners });
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // 事件处理
+  handleSearchClick: function() {
+    console.log("点击了搜索框");
+    wx.navigateTo({
+      url: '/pages/detail-search/index',
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  handleSwiperImageLoading: function() {
+    // 获取图片的高度(如何去获取某一个组件的高度)
+    queryRect(".swiper-image").then(res => {
+      const rect = res[0];
+      this.setData({ swiperHeight: rect.height });
+    });
   },
 
   /**
@@ -42,25 +50,4 @@ Page({
   onUnload: function () {
 
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })

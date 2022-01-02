00f43c7a486cdd277f52aac97b42993fd0a916f7
Emma-xiaochen <736202625@qq.com>
2022-01-02 15:05:47 +0800

    状态管理

--- service/api_video.js
+++ service/api_video.js
@@ -6,3 +6,33 @@ export function getTopMV(offset, limit = 10) {
     limit
   })
 }
+
+/**
+ * 请求MV的播放地址
+ * @param {number} id MV的id
+ */
+export function getMVURL(id) {
+  return cmRequest.get("/mv/url", {
+    id
+  })
+}
+
+/**
+ * 请求MV数据
+ * @param {number} mvid MV的id
+ */
+export function getMVDetail(mvid) {
+  return cmRequest.get("/mv/detail", {
+    mvid
+  })
+}
+
+/**
+ * 请求相关视频
+ * @param {number} id 视频的id
+ */
+export function getRelatedVideo(id) {
+  return cmRequest.get("/related/allvideo", {
+    id
+  })
+}

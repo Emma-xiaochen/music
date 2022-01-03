55be141a001cfd88bc41b155ef45e3c7195586bd
Emma-xiaochen <736202625@qq.com>
2022-01-03 19:51:19 +0800

    音乐首页搭建和开发

--- service/api_music.js
+++ service/api_music.js
@@ -1,7 +1,15 @@
 import cmRequest from './index'
 
+// 轮播图接口
 export function getBanners() {
   return cmRequest.get("/banner", {
     type: 2
+  })
+}
+
+// 歌曲排行接口
+export function getRanking() {
+  return cmRequest.get("/top/list", {
+    idx: 1
   })
 }

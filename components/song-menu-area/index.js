4447b4e1fcbc21d2f9ddbe203521833dc57cffcf
Emma-xiaochen <736202625@qq.com>
2022-01-04 23:06:41 +0800

    首页开发

---
+++ components/song-menu-area/index.js
@@ -0,0 +1,37 @@
+// components/song-menu-area/index.js
+const app = getApp();
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: {
+      type: String,
+      value: "热门歌单"
+    },
+    songMenu: {
+      type: Array,
+      value: []
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    
+    screenWidth: app.globalData.screenWidth,
+    screenHeight: app.globalData.screenHeight
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    demo: function() {
+      const info = wx.getSystemInfoAsync();
+      screenWidth
+    }
+  }
+})

8ad084d9266f96aafe10953bfd1a8a1cc2037cda
Emma-xiaochen <736202625@qq.com>
2022-01-06 18:16:06 +0800

    歌单跳转&歌单数据获取&展示

--- pages/home-music/index.js
+++ pages/home-music/index.js
@@ -85,7 +85,7 @@ Page({
 
   navigateToDetailSongsPage: function(rankingName) {
     wx.navigateTo({
-      url: `/pages/detail-songs/index?ranking=${rankingName}`
+      url: `/pages/detail-songs/index?ranking=${rankingName}$type=rank`
     })
   },
 

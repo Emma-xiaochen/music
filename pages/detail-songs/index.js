5c3cde7a46aa8d7c8c79437bb1653fca3fdab856
Emma-xiaochen <736202625@qq.com>
2022-01-06 18:15:51 +0800

    歌单跳转&歌单数据获取&展示

--- pages/detail-songs/index.js
+++ pages/detail-songs/index.js
@@ -1,5 +1,6 @@
 // pages/detail-songs/index.js
 import { rankingStore } from '../../store/index'
+import { getSongMenuDetail } from '../../service/api_music'
 
 Page({
 
@@ -7,28 +8,42 @@ Page({
    * 页面的初始数据
    */
   data: {
+    type: "",
     ranking: "",
-    rankingInfo: {}
+    songInfo: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    const ranking = options.ranking;
-    this.setData({ ranking });
-    // 1. 获取数据
-    rankingStore.onState(ranking, this.getRankingDataHandler)
+    const type = options.type;
+    this.setData({ type });
+    if(type === "menu") {
+      const id = options.id;
+      console.log(id);
+      getSongMenuDetail(id).then(res => {
+        console.log(res);
+        this.setData({ songInfo: res.playlist })
+      })
+    } else if(type === "rank") {
+      const ranking = options.ranking;
+      this.setData({ ranking });
+      // 1. 获取数据
+      rankingStore.onState(ranking, this.getRankingDataHandler)
+    }
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    rankingStore.offState(this.data.ranking, this.getRankingDataHandler);
+    if(this.data.ranking) {
+      rankingStore.offState(this.data.ranking, this.getRankingDataHandler);
+    }
   },
 
   getRankingDataHandler: function(res) {
-    this.setData({ rankingInfo: res });
+    this.setData({ songInfo: res });
   }
 })

b9a9b96e0e5c57fe6016cc68fe5bde796fb0020b
Emma-xiaochen <736202625@qq.com>
2022-01-12 16:36:11 +0800

    封装自定义导航栏

--- baseui/nav-bar/index.js
+++ baseui/nav-bar/index.js
@@ -1,17 +1,31 @@
 // baseui/nav-bar/index.js
 Component({
+  options: {
+    multipleSlots: true
+  },
   /**
    * 组件的属性列表
    */
   properties: {
-
+    title: {
+      type: String,
+      value: "默认标题"
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
+    statusBarHeight: getApp().globalData.statusBarHeight
+  },
 
+  lifetimes: {
+    ready: function() {
+      console.log(1);
+      const info = wx.getSystemInfoSync();
+      console.log(info);
+    }
   },
 
   /**

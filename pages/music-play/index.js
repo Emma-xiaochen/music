4af9157b266527d938c94724f76ff25786f2d8ff
Emma-xiaochen <736202625@qq.com>
2022-01-12 17:26:37 +0800

    播放页面分页布局

--- pages/music-play/index.js
+++ pages/music-play/index.js
@@ -1,4 +1,5 @@
 // pages/music-play/index.js
+import { NavBarHeight } from '../../constants/device-const';
 import { getSongDetail } from '../../service/api_player'
 
 Page({
@@ -8,7 +9,9 @@ Page({
    */
   data: {
     id: 0,
-    currentSong: {}
+    currentSong: {},
+    currentPage: 0,
+    contentHeight: 0
   },
 
   /**
@@ -21,6 +24,14 @@ Page({
 
     // 2. 根据id获取歌曲信息
     this.getPageData(id);
+
+    // 3. 动态计算内容高度
+    const globalData = getApp().globalData;
+    const screenHeight = getApp().globalData.screenHeight;
+    const statusBarHeight = getApp().globalData.statusBarHeight;
+    const navBarHeight = globalData.navBarHeight;
+    const contentHeight = screenHeight - statusBarHeight - navBarHeight;
+    this.setData({ contentHeight });
   },
 
   // 网络请求
@@ -29,6 +40,13 @@ Page({
       console.log(res);
       this.setData({ currentSong: res.songs[0] })
     })
+  },
+
+  // 事件处理
+  handleSwiperChange: function(event) {
+    console.log(event);
+    const current = event.detail.current;
+    this.setData({ currentPage: current });
   },
 
   /**

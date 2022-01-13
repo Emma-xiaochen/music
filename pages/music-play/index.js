460d190c59c225d1355efca25c94007099a4cadb
Emma-xiaochen <736202625@qq.com>
2022-01-13 18:14:36 +0800

    播放页面界面数据&audioContext优化

--- pages/music-play/index.js
+++ pages/music-play/index.js
@@ -1,6 +1,6 @@
 // pages/music-play/index.js
-import { NavBarHeight } from '../../constants/device-const';
 import { getSongDetail } from '../../service/api_player'
+import { audioContext } from '../../store/index'
 
 Page({
 
@@ -10,6 +10,7 @@ Page({
   data: {
     id: 0,
     currentSong: {},
+    duration: 0,
     isMusicLyric: true,
     currentPage: 0,
     contentHeight: 0
@@ -35,16 +36,19 @@ Page({
     const contentHeight = screenHeight - statusBarHeight - navBarHeight;
     this.setData({ contentHeight, isMusicLyric: deviceRadio >= 2 });
 
-    // 4. 创建播放器
-    const audioContext = wx.createInnerAudioContext();
+    // 4. 使用audioContext播放歌曲
+    audioContext.stop();
     audioContext.src = `https://music.163.com/song/media/outer/url?id=${id}.mp3`;
-    audioContext.play();
+    audioContext.autoplay = true;
+    audioContext.onCanplay(() => {
+      audioContext.play();
+    })
   },
 
   // 网络请求
   getPageData: function(id) {
     getSongDetail(id).then(res => {
-      this.setData({ currentSong: res.songs[0] })
+    this.setData({ currentSong: res.songs[0], duration: res.songs[0].dt })
     })
   },
 

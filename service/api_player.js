78d2c22fcb10f3e8d1d587b540ea8fa59d743a96
Emma-xiaochen <736202625@qq.com>
2022-01-14 17:06:36 +0800

    播放页面请求和解析歌词的过程

--- service/api_player.js
+++ service/api_player.js
@@ -1,7 +1,15 @@
 import cmRequest from './index'
 
+// 歌曲详情接口
 export function getSongDetail(ids) {
   return cmRequest.get("/song/detail", {
     ids
+  })
+}
+
+// 歌词信息
+export function getSongLyric(id) {
+  return cmRequest.get('/lyric', {
+    id
   })
 }

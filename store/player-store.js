c7843435f0343f822f750b829f645edd08ba9c26
Emma-xiaochen <736202625@qq.com>
2022-01-15 18:49:40 +0800

    播放页面的播放监听逻辑的抽取

--- store/player-store.js
+++ store/player-store.js
@@ -10,6 +10,10 @@ const playerStore = new cmEventStore({
     currentSong: {}, // 当前歌曲
     durationTime: 0, // 歌曲总时长
     lyricInfos: [], // 歌词数据
+
+    currentTime: 0, // 当前播放的时间
+    currentLyricIndex: 0, // 当前歌词索引
+    currentLyricText: "", // 当前歌词文本
   },
   actions: {
     playMusicWithSongIdAction(ctx, { id }) {
@@ -32,6 +36,43 @@ const playerStore = new cmEventStore({
       audioContext.stop();
       audioContext.src = `https://music.163.com/song/media/outer/url?id=${id}.mp3`;
       audioContext.autoplay = true;
+
+      // 3. 监听audioContext一些事件
+      this.dispatch("setupAudioContextListenerAction");
+    },
+    setupAudioContextListenerAction(ctx) {
+      // 1. 监听歌曲可以播放
+      audioContext.onCanplay(() => {
+        audioContext.play();
+      });
+  
+      // 2. 监听当前时间改变
+      audioContext.onTimeUpdate(() => {
+        // 1. 获取当前时间
+        const currentTime = audioContext.currentTime * 1000;
+  
+        // 2. 根据当前时间修改currentTime
+        ctx.currentTime = currentTime;
+        
+  
+        //  3. 根据当前时间去查找播放的歌词
+        if(!ctx.lyricInfos.length) return;
+        let i = 0;
+        for(; i < ctx.lyricInfos.length; i++) {
+          const lyricInfo = ctx.lyricInfos[i];
+          if(currentTime < lyricInfo.time) {
+            break;
+          }
+        }
+  
+        // 设置当前歌词的索引和内容
+        const currentIndex = i - 1;
+        if(ctx.currentLyricInfo !== currentIndex) {
+          const currentLyricInfo = ctx.lyricInfos[currentIndex];
+          ctx.currentLyricIndex = currentIndex;
+          ctx.currentLyricText = currentLyricInfo.text;
+        }
+      });
     }
   }
 });

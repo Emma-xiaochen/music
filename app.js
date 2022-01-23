1263ce95a71e7f89759ae171ccffe23d0cbec795
Emma-xiaochen <736202625@qq.com>
2022-01-24 00:56:59 +0800

    登录代码的封装&登陆状态&登录判断

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 // app.js
-import { getLoginCode, codeToToken } from './service/api_login'; 
+import { 
+  getLoginCode, 
+  codeToToken, 
+  checkToken, 
+  checkSession 
+} from './service/api_login'; 
+import { TOKEN_KEY } from './constants/token-const'
+
 App({
   globalData: {
     screenWidth: 0,
@@ -8,7 +15,7 @@ App({
     navBarHeight: 44,
     deviceRadio: 0
   },
-  onLaunch: function() {
+  onLaunch: async function() {
     // 1. 获取了设备信息
     const info = wx.getSystemInfoSync();
     this.globalData.screenWidth = info.screenWidth;
@@ -19,15 +26,24 @@ App({
     this.globalData.deviceRadio = deviceRadio;
 
     //  2. 让用户默认进行登录
-    this.loginAction();
+    const token = wx.getStorageSync(TOKEN_KEY);
+    // 验证token有没有过期
+    const checkResult = await checkToken(token);
+    console.log(checkResult);
+    // 判断session是否过期
+    const isSessionExpire = await checkSession();
+
+    if(!token || checkResult.errorCode || !isSessionExpire) {
+      this.loginAction();
+    }
   },
   loginAction: async function() {
     // 1.获取code
     const code = await getLoginCode();
-    console.log(code);
 
     // 2. 将code发送给服务器
     const result = await codeToToken(code);
-    console.log(result);
+    const token = result.token;
+    wx.setStorageSync(TOKEN_KEY, token); 
   }
 })

0457e5714c62bd6816a11ed8dc85920da51af120
Emma-xiaochen <736202625@qq.com>
2022-01-23 23:59:29 +0800

    登录

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 // app.js
+import { getLoginCode, codeToToken } from './service/api_login'; 
 App({
+  globalData: {
+    screenWidth: 0,
+    screenHeight: 0,
+    statusBarHeight: 0,
+    navBarHeight: 44,
+    deviceRadio: 0
+  },
   onLaunch: function() {
+    // 1. 获取了设备信息
     const info = wx.getSystemInfoSync();
     this.globalData.screenWidth = info.screenWidth;
     this.globalData.screenHeight = info.screenHeight;
@@ -8,12 +17,17 @@ App({
 
     const deviceRadio = info.screenHeight / info.screenWidth;
     this.globalData.deviceRadio = deviceRadio;
+
+    //  2. 让用户默认进行登录
+    this.loginAction();
   },
-  globalData: {
-    screenWidth: 0,
-    screenHeight: 0,
-    statusBarHeight: 0,
-    navBarHeight: 44,
-    deviceRadio: 0
+  loginAction: async function() {
+    // 1.获取code
+    const code = await getLoginCode();
+    console.log(code);
+
+    // 2. 将code发送给服务器
+    const result = await codeToToken(code);
+    console.log(result);
   }
 })

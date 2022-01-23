897e8f943950f82c8d803aca8dba0b0c7cdf410b
Emma-xiaochen <736202625@qq.com>
2022-01-24 00:57:33 +0800

    登录代码的封装&登陆状态&登录判断

--- service/index.js
+++ service/index.js
@@ -1,34 +1,38 @@
 const BASE_URL = "http://123.207.32.32:9001";
 
+// 已经部署好的
 const LOGIN_BASE_URL = "http://123.207.32.32:3000";
+// 登录服务器代码,自己部署
+// const LOGIN_BASE_URL = "http://localhost:3000"
 
 class CMRequest {
   constructor(baseURL) {
     this.baseURL = baseURL;
   }
   
-  request(url, method, params) {
+  request(url, method, params, header = {}) {
     return new Promise((resolve, reject) => {
       wx.request({
         url: this.baseURL + url,
         method: method,
+        header: header,
         data: params, 
         success: (res) => {
           resolve(res.data);
         },
-        fail: reject
-        // fail: (err) => {
-        //   reject(err);
-        // }
+        // fail: reject
+        fail: (err) => {
+          reject(err);
+        }
       })
     })
   }
-  get(url, params) {
-    return this.request(url, "GET", params)
+  get(url, params, header) {
+    return this.request(url, "GET", params, header);
   }
 
-  post(url, data) {
-    return this.request(url, "POST", data)
+  post(url, data, header) {
+    return this.request(url, "POST", data, header);
   }
 }
 
@@ -39,4 +43,4 @@ const cmLoginRequest = new CMRequest(LOGIN_BASE_URL);
 export default cmRequest;
 export{
   cmLoginRequest
-};
+}

8f4fd3af128388fd52758dbc440807839359efe0
Emma-xiaochen <736202625@qq.com>
2022-01-24 19:03:26 +0800

    登录代码的优化

--- service/index.js
+++ service/index.js
@@ -1,3 +1,7 @@
+import { TOKEN_KEY } from '../constants/token-const';
+
+const token = wx.getStorageSync(TOKEN_KEY);
+
 const BASE_URL = "http://123.207.32.32:9001";
 
 // 已经部署好的
@@ -6,16 +10,19 @@ const LOGIN_BASE_URL = "http://123.207.32.32:3000";
 // const LOGIN_BASE_URL = "http://localhost:3000"
 
 class CMRequest {
-  constructor(baseURL) {
+  constructor(baseURL, authHeader = {}) {
     this.baseURL = baseURL;
+    this.authHeader = authHeader;
   }
   
-  request(url, method, params, header = {}) {
+  request(url, method, params, isAuth = false, header = {}) {
+    const finalHeader = isAuth ? { ...this.authHeader, header } : header;
+
     return new Promise((resolve, reject) => {
       wx.request({
         url: this.baseURL + url,
         method: method,
-        header: header,
+        header: finalHeader,
         data: params, 
         success: (res) => {
           resolve(res.data);
@@ -27,18 +34,20 @@ class CMRequest {
       })
     })
   }
-  get(url, params, header) {
-    return this.request(url, "GET", params, header);
+  get(url, params, isAuth = false, header) {
+    return this.request(url, "GET", params, isAuth, header);
   }
 
-  post(url, data, header) {
-    return this.request(url, "POST", data, header);
+  post(url, data, isAuth = false, header) {
+    return this.request(url, "POST", data, isAuth, header);
   }
 }
 
 const cmRequest = new CMRequest(BASE_URL);
 
-const cmLoginRequest = new CMRequest(LOGIN_BASE_URL);
+const cmLoginRequest = new CMRequest(LOGIN_BASE_URL, {
+  token
+});
 
 export default cmRequest;
 export{

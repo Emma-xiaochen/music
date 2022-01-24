5712585a11a3b0039b16c561056d682b15973ebd
Emma-xiaochen <736202625@qq.com>
2022-01-24 19:03:16 +0800

    登录代码的优化

--- pages/home-music/index.js
+++ pages/home-music/index.js
@@ -29,7 +29,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    playerStore.dispatch("playMusicWithSongIdAction", { id: 1842025914 })
+    // playerStore.dispatch("playMusicWithSongIdAction", { id: 1842025914 })
 
     // 获取页面数据
     this.getPageData();
